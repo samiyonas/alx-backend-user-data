e047957b6c02b4de627aca772bedb7af4f04b81a
samiyonas <samuel.x.yonas@gmail.com>
2024-09-30 16:19:34 -0700

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized_user():
+    """ aborts 401 error """
+    abort(401)
