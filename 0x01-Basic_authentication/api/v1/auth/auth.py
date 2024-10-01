7b71eaa0f2d9d0cfe4bea1d8b3b46d33e853a5aa
samiyonas <samuel.x.yonas@gmail.com>
2024-09-30 18:01:41 -0700

    routes that don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,17 @@ class Auth:
     """ the Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False """
-        return False
+        if path[len(path) - 1] == '/':
+            path1 = path[:len(path) - 1]
+        else:
+            path1 = path + '/'
+
+        if path not in excluded_paths or path1 not in excluded_paths:
+            return True
+        else:
+            return False
+        if not path or not excluded_paths:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None """
