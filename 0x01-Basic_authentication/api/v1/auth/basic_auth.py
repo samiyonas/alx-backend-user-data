3cdfe88a2284b2a009041bed2b81f2b61c7bf205
samiyonas <samuel.x.yonas@gmail.com>
2024-10-02 14:30:46 -0700

    boom

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,12 @@ class BasicAuth(Auth):
         if search_result[0].is_valid_password(user_pwd):
             return search_result[0]
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
