07d6c2e995bdddb59989dd34d6dc1afa6ed2ec93
samiyonas <samuel.x.yonas@gmail.com>
2024-10-05 01:24:18 -0700

    find user by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base
 from user import User
 
@@ -38,3 +39,16 @@ class DB:
         sesh.add(new_user)
         sesh.commit()
         return new_user
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """ search for user """
+        for key in kwargs:
+            if key not in User.__table__.columns:
+                raise InvalidRequestError()
+
+        session = self._session
+        found_user = session.query(User).filter_by(**kwargs).first()
+        if not found_user:
+            raise NoResultFound()
+        else:
+            return found_user
