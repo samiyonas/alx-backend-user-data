772fd01b83985c07896d8a09298abad458c6713b
samiyonas <samuel.x.yonas@gmail.com>
2024-10-06 16:52:54 -0700

    find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,8 +69,10 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None):
         """ get a user from a session id """
+        if not session_id:
+            return None
         try:
-            user = self._db.find_user_by(session_id=session_id)
-            return user
+            new_user = self._db.find_user_by(session_id=session_id)
+            return new_user
         except NoResultFound:
             return None
