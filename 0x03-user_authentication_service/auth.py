041d2ac9ece98b6b04e7d092f63838af65662cd7
samiyonas <samuel.x.yonas@gmail.com>
2024-10-06 17:32:20 -0700

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,7 +80,7 @@ class Auth:
             return None
 
         return user
-    
+
     def destroy_session(self, user_id: int) -> None:
         """ destroy a session """
         user = self._db.find_user_by(id=user_id)
