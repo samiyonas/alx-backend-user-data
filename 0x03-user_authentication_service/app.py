974ba05c6a6569b53c7c4b44e158bf92e3227435
samiyonas <samuel.x.yonas@gmail.com>
2024-10-07 16:22:00 -0700

    update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -110,7 +110,7 @@ def update_password():
         is_password_changed = False
     if not is_password_changed:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
